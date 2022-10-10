74960471b9daf37f682cf223e1a1ce9deb6f5ec0
Daniel Vuk <vuk.daniel@gmail.com>
2022-10-10 17:55:19 +0200

    Rijesen Zadatak 6

---
+++ 01-Zadaci/zadatak6.js
@@ -0,0 +1,24 @@
+/* 
+Napiši funkciju kojoj se predaje string, te ona ispisuje bez razmaka svaku
+rijec velikim slovom (cammelCase)
+Ispis : “web apps vjezbe” -> “webAppsVjezbe” 
+*/
+
+
+const toCammelCase = (str) =>{
+    str.toLowerCase()
+    let newStr = '';
+
+    for(let i = 0; i<str.length; i++){
+        if(str[i] == ' '){
+            newStr+= str[i+1].toUpperCase();
+            i++;
+        }else{
+            newStr+=(str[i].toLowerCase())
+        }   
+    }
+
+    console.log(newStr);
+}
+
+toCammelCase('web ApPs vjeZbe')
